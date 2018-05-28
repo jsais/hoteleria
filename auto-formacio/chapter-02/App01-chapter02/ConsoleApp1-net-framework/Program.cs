b978f80d91986988b8a5c7371bd2a26bc196db58
jsais <39535081+jsais@users.noreply.github.com>
2018-05-28 23:28:00 +0200

    Add files via upload

---
+++ auto-formacio/chapter-02/App01-chapter02/ConsoleApp1-net-framework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1_net_framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My Rocking App pepito";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("***********************************************");
+            Console.WriteLine("********** Welcome to My Rocking App **********");
+            Console.WriteLine("***********************************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Beep();
+            
+            // Espera a que s'hagi premut la tecla Enter
+            Console.ReadLine();
+            MessageBox.Show("All done!");
+        }
+    }
+}
---
+++ auto-formacio/chapter-03/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Console I/O *****");
+            GetUserData();
+            Console.ReadLine();
+        }
+        private static void GetUserData()
+        {
+            // Get name and age
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            string userAge = Console.ReadLine();
+
+            // Change echo color, just for fun
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            // Echo to the console
+            Console.WriteLine("Hello {0}! You ar {1} years old.", userName, userAge);
+
+            // Restore previous color.
+            Console.ForegroundColor = prevColor;
+        }
+    }
+}
